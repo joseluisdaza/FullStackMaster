243e8d8fb60020252ac890bdeebc0b7ed434978a
Jose Daza <jose.daza@outlook.com>
2025-03-09 17:11:47 -0400

    Agregando funcionalidad de giro a botones

--- 01_Intro/Tarea02_Javascript/script.js
+++ 01_Intro/Tarea02_Javascript/script.js
@@ -2,15 +2,17 @@
 const verDetallesButton = document.getElementById("verDetalles");
 const volverButton = document.getElementById("volver");
 const tarjeta = document.querySelector(".tarjeta");
+const frontal = document.getElementById("caraFrontal");
+const posterior = document.getElementById("caraPosterior");
 
 // Función para voltear la tarjeta
 verDetallesButton.addEventListener("click", () => {
-  // tarjeta.style.transform = "rotateY(180deg)"; // Voltear la tarjeta
-  alert("Click en detalles");
+  frontal.style.transform = "rotateY(180deg)";
+  posterior.style.transform = "rotateY(0deg)";
 });
 
 // Función para volver a la cara frontal
 volverButton.addEventListener("click", () => {
-  // alert("Click en ");
-  tarjeta.style.transform = "rotateY(0deg)"; // Volver a la cara inicial
+  posterior.style.transform = "rotateY(180deg)";
+  frontal.style.transform = "rotateY(0deg)";
 });
