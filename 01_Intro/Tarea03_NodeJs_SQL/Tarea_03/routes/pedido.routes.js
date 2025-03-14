abe9bec03dbef6febe104c7577dc2db8ed3c4cc5
Jose Daza <jose.daza@outlook.com>
2025-03-13 23:10:06 -0400

    Agregando controllers y routes para Pedido

--- 01_Intro/Tarea03_NodeJs_SQL/Tarea_03/routes/pedido.routes.js
+++ 01_Intro/Tarea03_NodeJs_SQL/Tarea_03/routes/pedido.routes.js
@@ -3,10 +3,16 @@ const router = express.Router();
 const {
   obtenerPedidos,
   crearPedido,
+  obtenerPedido,
+  actualizarPedido,
+  eliminarPedido,
 } = require("../controllers/pedido.controller");
 const { route } = require("./cliente.routes");
 
 router.get("/pedidos", obtenerPedidos);
 router.post("/pedidos", crearPedido);
+router.get("/pedidos/:id", obtenerPedido);
+router.put("/pedidos/:id", actualizarPedido);
+router.delete("/pedidos/:id", eliminarPedido);
 
 module.exports = router;
