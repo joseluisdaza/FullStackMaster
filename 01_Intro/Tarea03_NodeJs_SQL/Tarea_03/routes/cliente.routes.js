9c86831a7cdcabd49a3fb986d4351859d1dc124a
Jose Daza <jose.daza@outlook.com>
2025-03-13 22:28:35 -0400

    Agregando endpoints para Clientes

--- 01_Intro/Tarea03_NodeJs_SQL/Tarea_03/routes/cliente.routes.js
+++ 01_Intro/Tarea03_NodeJs_SQL/Tarea_03/routes/cliente.routes.js
@@ -3,10 +3,15 @@ const router = express.Router();
 const {
   obtenerClientes,
   crearCliente,
+  obtenerCliente,
+  actualizarCliente,
+  eliminarCliente,
 } = require("../controllers/cliente.controller");
 
 //Rutas para Clientes
+router.get("/clientes/:id", obtenerCliente);
 router.get("/clientes", obtenerClientes);
-router.post("clientes", crearCliente);
-
+router.post("/clientes", crearCliente);
+router.put("/clientes/:id", actualizarCliente);
+router.delete("/clientes/:id", eliminarCliente);
 module.exports = router;
